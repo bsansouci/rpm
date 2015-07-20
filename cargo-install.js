683bcb531715d77ada4ce9488f31984dacd916f9
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-07-20 00:25:55 -0400

    Added toml-js for parsing .toml file. Added update function.

--- cargo-install.js
+++ cargo-install.js
@@ -1,5 +1,6 @@
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
+const toml = require('toml-js');
 
 const args = process.argv;
 if(args.length === 2) {
@@ -9,8 +10,8 @@ if(args.length === 2) {
   const commandArgs = args.slice(3);
 
   switch (command) {
-    case "i":
-    case "install":
+    case 'i':
+    case 'install':
       request("https://crates.io/api/v1/crates?q=" + commandArgs.join('+') + "&page=1&per_page=10&_=" + Date.now(), (err, res, body) => {
         if(err) throw err;
 
@@ -26,31 +27,37 @@ if(args.length === 2) {
 
         const first = allCrates.crates[0];
         console.log(`Number of results: ${allCrates.meta.total} | Using: ${first.id} (${first.max_version}): ${first.description}`);
-
-        const file = fs.readFileSync('Cargo.toml', 'utf8');
-        const splittedFile = file.split('[dependencies]');
-        let retFile = "";
-        if(splittedFile.length === 1) {
-          retFile = dedent`${splittedFile[0]}
-
-            [dependencies]
-            ${first.id} = "${first.max_version}"
-          `;
-        } else {
-          retFile = dedent`${splittedFile[0]}
-
-            [dependencies]
-            ${splittedFile[1]}
-            ${first.id} = "${first.max_version}"
-          `;
+        if(first.id !== commandArgs.join(" ")) {
+          console.log(`Warning: install crate which name doesn\'t entirely match! "${first.id}" VS "${commandArgs.join(" ")}".`);
         }
 
+        const file = fs.readFileSync('Cargo.toml', 'utf8');
+        let parsedFile = toml.parse(file);
+
+        // Add dependency field if missing
+        if(!parsedFile.dependencies) parsedFile.dependencies = {};
+
+        // Check if dependency already exist, if so what version it's in
+        if(parsedFile.dependencies[first.id]) {
+          if(parsedFile.dependencies[first.id] !== first.max_version) {
+            console.log(`Dependency "${first.id}" exists already but is at version ${parsedFile.dependencies[first.id]} (latest is ${first.max_version}). Run "rpm u ${first.id}" to update.`);
+          } else {
+            console.log(`Dependency "${first.id}" up to date.`);
+          }
+          return; // Stop execution
+        }
+
+        // Add dependency
+        parsedFile.dependencies[first.id] = first.max_version;
+
+        // Convert back to string and write to file
+        const retFile = toml.dump(parsedFile);
         fs.writeFileSync('Cargo.toml', retFile);
-        console.log('Done');
+        console.log(`${first.id} install successfully.`);
       });
       break;
-    case "s":
-    case "search":
+    case 's':
+    case 'search':
       request("https://crates.io/api/v1/crates?q=" + commandArgs.join('+') + "&page=1&per_page=10&_=" + Date.now(), (err, res, body) => {
         if(err) throw err;
 
@@ -60,14 +67,58 @@ if(args.length === 2) {
 
         const allCrates = JSON.parse(body);
         if(allCrates.meta.total === 0) {
-          console.log(`No result found for ${commandArgs.join(' ')}`);
+          console.log(`No result found for ${commandArgs.join(' ')}.`);
           return;
         }
-        console.log(`Total number of results: ${allCrates.meta.total}`);
+        console.log(`Total number of results: ${allCrates.meta.total}.`);
 
         allCrates.crates.forEach(crate => {
           console.log(`${crate.id} (${crate.max_version}): ${crate.description}`);
         });
+      });
+      break;
+    case 'u':
+    case 'update':
+      request("https://crates.io/api/v1/crates?q=" + commandArgs.join('+') + "&page=1&per_page=10&_=" + Date.now(), (err, res, body) => {
+        if(err) throw err;
+
+        if(res.statusCode !== 200) {
+          throw new Error("Error code wasn't 200: " + res.statusCode);
+        }
+
+        const allCrates = JSON.parse(body);
+        if(allCrates.meta.total === 0) {
+          console.log(`No result found for ${commandArgs.join(' ')}.`);
+          return;
+        }
+
+        const first = allCrates.crates[0];
+        console.log(`Number of results: ${allCrates.meta.total} | Using: ${first.id} (${first.max_version}): ${first.description}.`);
+        if(first.id !== commandArgs.join(" ")) {
+          console.log(`Warning: install crate which name doesn\'t entirely match! "${first.id}" VS "${commandArgs.join(" ")}".`);
+        }
+
+        const file = fs.readFileSync('Cargo.toml', 'utf8');
+        let parsedFile = toml.parse(file);
+
+        // Add dependency field if missing
+        if(!parsedFile.dependencies || !parsedFile.dependencies[first.id]) {
+          console.log(`Dependency "${first.id}" doesn't exist. Run "rpm i ${first.id}" to install it.`);
+          return; // Stop execution
+        }
+
+        if(parsedFile.dependencies[first.id] === first.max_version) {
+          console.log(`Dependency "${first.id}" up to date.`);
+          return; // Stop execution
+        }
+
+        // Add dependency
+        parsedFile.dependencies[first.id] = first.max_version;
+
+        // Convert back to string and write to file
+        const retFile = toml.dump(parsedFile);
+        fs.writeFileSync('Cargo.toml', retFile);
+        console.log(`${first.id} updated successfully to ${first.max_version}.`);
       });
       break;
     default:
